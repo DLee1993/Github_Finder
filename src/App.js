df423bc2f0086fe6a4f5fbbc4310b1a7ac37b72e
DLee1993 <Dai180293@gmail.com>
2022-10-25 19:43:50 +0100

    looped through users array and added each of them to page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
-import UserItem from './components/users/userItem'; 
+import User from "./components/users/Users";
 import "./App.css";
+import Users from "./components/users/Users";
 
 class App extends Component {
     render() {
         return (
             <div className='App'>
-                <Navbar/>
-                <UserItem />
+                <Navbar />
+                <div className='container'>
+                    <Users />
+                </div>
             </div>
         );
     }
