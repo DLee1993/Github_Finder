adb3aafd0c9ab1c161728524c4c5cbea39303408
DLee1993 <Dai180293@gmail.com>
2022-10-28 19:14:39 +0100

    added components, state and props

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
 
+class App extends Component {
+    state = {
+        users: [],
+        loading: false,
+    };
+    async componentDidMount() {
+        try {
+            this.setState({ loading: true });
+            const res = await axios.get(
+                `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+            );
+            this.setState({ users: res.data, loading: false });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    render() {
+        const { loading, users } = this.state;
+        return (
+            <div className='App'>
+                <Navbar />
+                <div className='container'>
+                    <Users loading={loading} users={users} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
