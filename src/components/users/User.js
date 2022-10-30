251cb92fa762bd184bf2ad65a41a6618af105803
DLee1993 <Dai180293@gmail.com>
2022-10-30 23:27:32 +0000

    added all user information to user page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,27 +1,104 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, Fragment } from "react";
+import { useParams, Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function User({ user, loading, getUser }) {
+function User({ user, getUser }) {
     const { login } = useParams();
     const {
         name,
-        // avatar_url,
-        // location,
-        // bio,
-        // blog,
-        // html_url,
-        // followers,
-        // following,
-        // public_repos,
-        // piublic_gists,
-        // hireable,
+        avatar_url,
+        location,
+        company,
+        bio,
+        blog,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable,
     } = user;
 
     useEffect(() => {
         getUser(login);
     }, [getUser, login]);
 
-    return <div>{name}</div>;
+    return (
+        <Fragment>
+            <Link to='/' className='btn btn-light'>
+                Back to Search
+            </Link>
+            <div className='card grid-2'>
+                <div className='all-center'>
+                    <img
+                        src={avatar_url}
+                        alt='profile'
+                        className='round-img'
+                        style={{ width: "150px" }}
+                    />
+                    <h1>{name}</h1>
+                    <p>Location: {location}</p>
+                    <div>
+                        Hireable:{" "}
+                        {hireable ? (
+                            <i className='fas fa-check text-success' />
+                        ) : (
+                            <i className='fas fa-times-circle text-danger' />
+                        )}
+                    </div>
+                </div>
+                <div>
+                    {bio && (
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                    )}
+                    <a href={html_url} className='btn btn-dark my-1'>
+                        Visit Github Profile
+                    </a>
+                    <ul>
+                        <li>
+                            {login && (
+                                <Fragment>
+                                    <strong>Username: </strong>
+                                    {login}
+                                </Fragment>
+                            )}
+                        </li>
+                        <li>
+                            {company && (
+                                <Fragment>
+                                    <strong>Company: </strong>
+                                    {company}
+                                </Fragment>
+                            )}
+                        </li>
+                        <li>
+                            {blog && (
+                                <Fragment>
+                                    <strong>Website Blog: </strong>
+                                    {blog}
+                                </Fragment>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className='card text-center'>
+                <div className='badge badge-primary'>Followers: {followers}</div>
+                <div className='badge badge-success'>Following: {following}</div>
+                <div className='badge badge-light'>Public Repos: {public_repos}</div>
+                <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+            </div>
+        </Fragment>
+    );
 }
+
+User.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+};
 
 export default User;
